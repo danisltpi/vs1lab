e4c156440324ec653ea307cfb8dd372de68ab38c
Jannis <jajanau@gmail.com>
2022-04-27 19:50:00 +0200

    Aufgabe 2

--- Aufgabe2/gta_v2/public/javascripts/geotagging.js
+++ Aufgabe2/gta_v2/public/javascripts/geotagging.js
@@ -103,8 +103,45 @@ class MapManager {
  * It is called once the page has been fully loaded.
  */
 // ... your code here ...
+function updateLocation() {
+	function callback(helper) {
+		let latElem = document.getElementById("Latitude");
+		let longElem = document.getElementById("Longitude");
+		let latDiscElem = document.getElementById("LatitudeDisc");
+		let longDiscElem = document.getElementById("LongitudeDisc");
+		let mapElem = document.getElementById("mapView");
+
+		if (!latElem || !longElem || !latDiscElem || !longDiscElem || !mapElem) {
+			console.log("elements couldn't be retrieved by their IDs, check your HTML");
+            alert("elements couldn't be retrieved by their IDs, check your HTML");
+			return;
+		}
+
+		latElem.setAttribute("value", helper.latitude);
+		longElem.setAttribute("value", helper.longitude);
+		latDiscElem.setAttribute("value", helper.latitude);
+		longDiscElem.setAttribute("value", helper.longitude);
+
+		let manager = new MapManager("BOGAReBcjWRIADRh7GSjV9WPGeURXJwX");
+		if (!manager) {
+			console.log("failed to create map manager with user given API key");
+			return;
+		}
+
+		let maplink = manager.getMapUrl(helper.latitude, helper.longitude);
+		if (!maplink) {
+			console.log("failed to retrieve location image");
+			return;
+		}
+
+		mapElem.setAttribute("src", maplink);
+	}
+
+	LocationHelper.findLocation(callback);
+}
 
 // Wait for the page to fully load its DOM content, then call updateLocation
 document.addEventListener("DOMContentLoaded", () => {
-    alert("Please change the script 'geotagging.js'");
+	updateLocation();
 });
+
